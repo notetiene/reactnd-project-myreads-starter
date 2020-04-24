f32fe462bfc400ce1566067c81f7e8da93edb8fc
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-04-24 13:06:20 -0400

    Add SearchPage component
    * src/App.jsx: Remove search page elements
    * src/SearchPage.jsx: Add a stateless component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import * as BooksAPI from './BooksAPI'
 import './App.css';
+import SearchPage from './SearchPage';
 
 class BooksApp extends React.Component {
   constructor(props) {
@@ -17,6 +18,10 @@ class BooksApp extends React.Component {
     };
   }
 
+  handleCloseSearch = () => {
+    this.setState({ showSearchPage: false });
+  }
+
   render() {
     const {
       showSearchPage,
@@ -25,37 +30,9 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         {showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button
-                className="close-search"
-                onClick={() => this.setState({ showSearchPage: false })}
-                type="button"
-              >
-                Close
-              </button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method
-                  DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every
-                  search is limited by search terms.
-                */}
-                <input
-                  type="text"
-                  placeholder="Search by title or author"
-                />
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid" />
-            </div>
-          </div>
+          <SearchPage
+            onCloseSearch={this.handleCloseSearch}
+          />
         ) : (
           <div className="list-books">
             <div className="list-books-title">
