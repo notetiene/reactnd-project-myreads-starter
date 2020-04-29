8df01674313672fb5072d4f72c0615cea5586983
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-04-29 10:46:59 -0400

    Refactor SearchPage component to a class
    * src/SearchPage.jsx: Change the component to a class.  Add state with
    query property.

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -1,44 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function SearchPage(props) {
-  const {
-    onCloseSearch,
-  } = props;
+class SearchPage extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="search-books">
-      <div className="search-books-bar">
-        <button
-          className="close-search"
-          onClick={onCloseSearch}
-          type="button"
-        >
-          Close
-        </button>
-        <div className="search-books-input-wrapper">
-          {/*
-             NOTES: The search from BooksAPI is limited to a particular set of search terms.
-             You can find these search terms here:
-             https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+    this.state = {
+      query: '',
+    };
+  }
 
-             However, remember that the BooksAPI.search method
-             DOES search by title or author. So, don't worry if
-             you don't find a specific author or title. Every
-             search is limited by search terms.
-           */}
-          <input
-            type="text"
-            placeholder="Search by title or author"
-          />
+  render() {
+    const {
+      onCloseSearch,
+    } = this.props;
 
+    const {
+      query,
+    } = this.state;
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <button
+            className="close-search"
+            onClick={onCloseSearch}
+            type="button"
+          >
+            Close
+          </button>
+          <div className="search-books-input-wrapper">
+            {/*
+               NOTES: The search from BooksAPI is limited to a particular set of search terms.
+               You can find these search terms here:
+               https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+               However, remember that the BooksAPI.search method
+               DOES search by title or author. So, don't worry if
+               you don't find a specific author or title. Every
+               search is limited by search terms.
+             */}
+            <input
+              type="text"
+              placeholder="Search by title or author"
+            />
+
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid" />
         </div>
       </div>
-      <div className="search-books-results">
-        <ol className="books-grid" />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 SearchPage.propTypes = {
